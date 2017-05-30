a1bdddbe5352d6c81a5819a38ae220a3fce45a8a
SuperCuber <amit.gold00@gmail.com>
2017-05-30 18:36:54 +0300

    Bump version to 0.2.0 - Python was 0.1.0

--- src/args.rs
+++ src/args.rs
@@ -3,7 +3,7 @@ use clap;
 pub fn get_args() -> clap::ArgMatches<'static> {
     clap::App::new("Dotter")
         .setting(clap::AppSettings::SubcommandRequiredElseHelp)
-        .version("1.0.0")
+        .version("0.2.0")
         .author(crate_authors!())
         .about("A small dotfile manager.")
         .arg(clap::Arg::with_name("directory")
