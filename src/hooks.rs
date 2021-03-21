b23b0ee3dab7a5788fd95079755ac12128fbcbe3
Shizcow <pohl.devin@gmail.com>
2021-03-20 18:48:05 -0600

    Read/write to pipe success

--- src/hooks.rs
+++ src/hooks.rs
@@ -1,6 +1,9 @@
 use anyhow::{Context, Result};
 use handlebars::Handlebars;
+use interprocess::unnamed_pipe::pipe;
 
+use std::ffi::OsString;
+use std::io::Read;
 use std::path::Path;
 use std::process::Command;
 
@@ -31,14 +34,23 @@ pub(crate) fn run_hook(
     )
     .context("deploy script")?;
 
-    debug!("Running script file ");
+    debug!("Creating pipes ");
+    let (pipe_writer, mut pipe_reader) = pipe()?;
+
+    debug!("Running script file");
     let mut child = if cfg!(windows) {
         Command::new(script_file)
             .spawn()
             .context("spawn batch file")?
     } else {
+        use std::os::unix::io::AsRawFd;
+
+        let mut command_string = OsString::from(". ");
+        command_string.push(script_file);
+        command_string.push(format!("\nprintenv -0 >&{}", pipe_writer.as_raw_fd()));
         Command::new("sh")
-            .arg(script_file)
+            .arg("-c")
+            .arg(&command_string)
             .spawn()
             .context("spawn shell")?
     };
@@ -47,6 +59,44 @@ pub(crate) fn run_hook(
         child.wait().context("wait for child shell")?.success(),
         "subshell returned error"
     );
+
+    {
+        let _drop = pipe_writer;
+    }
+
+    // scoop up env vars
+    let env_vars: Vec<(OsString, OsString)> = if cfg!(windows) {
+        todo!();
+    } else {
+        let mut pipe_output = vec![];
+        pipe_reader.read_to_end(&mut pipe_output)?;
+        pipe_output.remove(pipe_output.len() - 1); // This is guarenteed to be a null character
+
+        pipe_output
+            .into_iter()
+            .fold(vec![vec![]], |mut acc, c| {
+                // Because we used printenv -0, everything is separated by null characters
+                // Just need to separate on those
+                if c == 0 {
+                    acc.push(vec![]);
+                } else {
+                    acc.last_mut().unwrap().push(c);
+                }
+                acc
+            })
+            .into_iter()
+            .map(|v| {
+                // Now to separate the names from the values
+                let envp = std::str::from_utf8(&v).unwrap();
+                // posix compliance states that the seperator between env names and env values is the '=' character
+                let i: Vec<_> = envp.splitn(2, "=").collect();
+                (
+                    i[0].to_string(),
+                    i.get(1).map(|s| s.to_string()).unwrap_or(String::new()),
+                )
+            })
+            .collect::<Vec<_>>()
+    };
 
     Ok(())
 }
