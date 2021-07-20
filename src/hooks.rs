0d0d7e49adde3d112415a59b75469a6101af82ad
PlayerNameHere <dixonseanlow@protonmail.com>
2021-07-20 16:19:22 +0800

    Check if hook needs to be rendered

--- src/hooks.rs
+++ src/hooks.rs
@@ -4,6 +4,8 @@ use handlebars::Handlebars;
 use std::path::Path;
 use std::process::Command;
 
+use crate::filesystem::{Filesystem, RealFilesystem};
+
 pub(crate) fn run_hook(
     location: &Path,
     cache_dir: &Path,
@@ -15,23 +17,35 @@ pub(crate) fn run_hook(
         return Ok(());
     }
 
-    let mut script_file = cache_dir.join(location);
-    if cfg!(windows) {
-        script_file.set_extension("bat");
+    let fs: &mut dyn Filesystem = &mut RealFilesystem::new(false);
+
+    // Default to current location
+    let mut script_file = location.into();
+
+    // If it is templated, then render it into cache and run from there
+    if fs
+        .is_template(location)
+        .context(format!("check whether {:?} is a template", location))?
+    {
+        script_file = cache_dir.join(location);
+        if cfg!(windows) {
+            script_file.set_extension("bat");
+        }
+
+        debug!("Rendering script {:?} -> {:?}", location, script_file);
+
+        crate::actions::perform_template_deploy(
+            location,
+            &script_file,
+            &std::env::temp_dir().join("dotter_temp").into(),
+            fs,
+            handlebars,
+            variables,
+        )
+        .context("deploy script")?;
     }
-    debug!("Rendering script {:?} -> {:?}", location, script_file);
 
-    crate::actions::perform_template_deploy(
-        location,
-        &script_file,
-        &std::env::temp_dir().join("dotter_temp").into(),
-        &mut crate::filesystem::RealFilesystem::new(false),
-        handlebars,
-        variables,
-    )
-    .context("deploy script")?;
-
-    debug!("Running script file ");
+    debug!("Running script file");
     let mut child = if cfg!(windows) {
         Command::new(script_file)
             .spawn()
