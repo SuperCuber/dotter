7b9995568f22f3bef2bdc857323796d3068e8992
Diomendius <42310725+Diomendius@users.noreply.github.com>
2024-04-12 17:11:28 +1200

    Avoid mysterious `Default::default()`
    In `deploy::test::{low_level_simple, low_level_skip}`,
    `Default::default()` was used to construct a `BTreeMap`, with the only
    indication of the variable's type being the signature of
    `RealActionRunner::new()`, which it was passed by reference to.
    
    Clippy's default-trait-access lint caught this.

--- src/deploy.rs
+++ src/deploy.rs
@@ -697,7 +697,7 @@ mod test {
 
         let opt = Options::default();
         let handlebars = handlebars::Handlebars::new();
-        let variables = Default::default();
+        let variables = BTreeMap::new();
 
         // Expectation:
         // create_symlink
@@ -800,7 +800,7 @@ mod test {
 
         let opt = Options::default();
         let handlebars = handlebars::Handlebars::new();
-        let variables = Default::default();
+        let variables = BTreeMap::new();
 
         // Expectation:
         // create_symlink
